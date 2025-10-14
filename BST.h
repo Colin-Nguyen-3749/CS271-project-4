33b0cc27de53fb6d4c5026c10e64979f9d5cd4a5
cnguyen3749 <colin.nguyen.3749@gmail.com>
2025-10-14 10:17:39 -0400

    Add boilerplate text to .h file

--- BST.h
+++ BST.h
@@ -5,3 +5,13 @@
 // This file contains all the method 
 // declarations of the BST class
 //===========================================
+
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <string>
+
+#ifndef BST_H
+#define BST_H
+
+using namespace std;
