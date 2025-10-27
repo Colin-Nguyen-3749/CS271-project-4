252346ddceb4187ecf03ddbeaf8b73acaf2fd67f
cnguyen3749 <colin.nguyen.3749@gmail.com>
2025-10-27 14:20:13 -0400

    Add two test cases to bst_test.cpp

--- bst_test.cpp
+++ bst_test.cpp
@@ -9,12 +9,11 @@
 #include <stdlib.h>
 #include <iostream>
 #include <math.h>
-#include "usecase.cpp"
 #include "BST.h"
 
 using namespace std;
 
-void test_insert1()
+void test_insert_CS_classes()
 {
     try
     {
@@ -25,31 +24,23 @@ void test_insert1()
         {
             cout << "Incorrect result of inserting (\"CS\", 271). Expected 271 but got : " << bst_str << endl;
         }
-        bst.insert("CS", 109);
-        bst.insert("CS", 111);
-        bst.insert("CS", 112);
+
         bst.insert("CS", 113);
-        bst.insert("CS", 114);
-        bst.insert("CS", 173);
-        bst.insert("CS", 181);
-        bst.insert("CS", 234);
-        bst.insert("CS", 395);
         bst.insert("CS", 371);
+        bst.insert("CS", 112);
+        bst.insert("CS", 395);
+        bst.insert("CS", 111);
+        bst.insert("CS", 114);
+        bst.insert("CS", 234);
+        bst.insert("CS", 181);
+        bst.insert("CS", 173);
+        bst.insert("CS", 109);
+       
         bst_str = bst.to_string();
-        if (bst_str != "271 109 395 111 371 112 113 114 173 181 234")
+
+        if (bst_str != "271 113 371 112 114 395 111 234 109 181 173")
         {
             cout << "Incorrect result of inserting keys in order. Expected 109 111 112 113 114 173 181 234 395 371 but got : " << bst_str << endl;
-        }
-        int vals[10] = {3, 7, 4, 5, 6, 9, 0, 23, 12, 18};
-        BST<string, int> balanced_bst;
-        for (int i = 0; i < 10; i++)
-        {
-            balanced_bst.insert("some data", vals[i]);
-        }
-        bst_str = balanced_bst.to_string();
-        if (bst_str != "3 0 7 4 9 5 23 6 12 18")
-        {
-            cout << "Incorrect result of inserting keys {3, 7, 4, 5, 6, 9, 0, 23, 12, 18}. Expected 3 0 7 4 9 5 23 6 12 18 but got : " << bst_str << endl;
         }
     }
     catch (exception &e)
@@ -57,6 +48,50 @@ void test_insert1()
         cerr << "Error inserting into bst : " << e.what() << endl;
     }
 }
+
+void test_insert_long()
+{
+    try
+    {
+        BST<string, int> bst;
+        bst.insert("Alfa", 1583124523);
+        string bst_str = bst.to_string();
+        if (bst_str != "1583124523")
+        {
+            cout << "Incorrect result of inserting (\"Alfa\", 1583124523). Expected 1583124523 but got : " << bst_str << endl;
+        }
+
+        bst.insert("Bravo", 13479128);
+        bst.insert("Charlie", 17429);
+        bst.insert("Delta", 8901726);
+        bst.insert("Echo", 1063801);
+        bst.insert("Foxtrot", 1098173849);
+        bst.insert("Golf", 14307);
+        bst.insert("Hotel", 983147);
+        bst.insert("India", 134563647);
+        bst.insert("Juliett", 54876);
+        bst.insert("Kilo", 314987);
+        bst.insert("Lima", 909735);
+        bst.insert("Mike", 1349807);
+        bst.insert("November", 23487);
+        bst.insert("Oscar", 3291);
+        bst.insert("Papa", 23491235);
+        bst.insert("Quebec", 92379852);
+
+
+        bst_str = bst.to_string();
+
+        if (bst_str != "1583124523 13479128 17429 1098173849 14307 8901726 134563647 3291 1063801 23491235 983147 1349807 92379852 54876 23487 314987 909735")
+        {
+            cout << "Incorrect result of inserting keys in order. Expected 1583124523 13479128 17429 1098173849 14307 8901726 134563647 1063801 983147 1349807 54876 23487 314987 909735 but got : " << bst_str << endl;
+        }
+    }
+    catch (exception &e)
+    {
+        cerr << "Error inserting into bst : " << e.what() << endl;
+    }
+}
+
 
 int main()
 {
@@ -67,7 +102,8 @@ int main()
          << endl;
 
     // test_empty();
-    test_insert1();
+    test_insert_CS_classes();
+    test_insert_long();
     // test_get();
     // test_remove();
     // test_max_data();
--- makefile
+++ makefile
@@ -1,2 +1,5 @@
-BST_test: usecase.cpp BST.h
-	g++ -o BST_test usecase.cpp
+# BST_test: usecase.cpp BST.h
+# 	g++ -o BST_test usecase.cpp
+
+BST_test: bst_test.cpp BST.h
+	g++ -o BST_test bst_test.cpp
